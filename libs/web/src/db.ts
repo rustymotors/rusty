73bb054d02c2c9f147eaf4e7771b618c78360659
Molly Draven <drazi@duck.com>
2024-07-23 20:49:47 -0400

    chore: Update Sequelize database configuration and add authentication logging

--- libs/web/src/db.ts
+++ libs/web/src/db.ts
@@ -3,4 +3,9 @@ import { Sequelize } from "sequelize";
 export const db = new Sequelize({
   dialect: "sqlite",
   storage: "db.sqlite",
+  logging: console.log, // Enable logging
 });
+
+db.authenticate()
+  .then(() => console.log("Database connected..."))
+  .catch((err) => console.error("Error connecting to the database:", err));
