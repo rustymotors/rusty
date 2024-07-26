c59222617a6500526c0e3428da5e9d8a5b4042e0
Molly Draven <drazi@duck.com>
2024-07-26 08:11:09 -0400

    refactor: Add customerId property to User model and AuthLogin service

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,27 @@
+// IMPORTANT: Make sure to import `instrument.js` at the top of your file.
+// If you're using ECMAScript Modules (ESM) syntax, use `import "./instrument.js";`
+void import ("./instrument.js");
+
+// All other imports below
+import * as Sentry from "@sentry/node";
 import { ServerController } from "./ServerController.js";
 
-const serverController = new ServerController();
-void serverController.start();
+process.on("unhandledRejection", (reason, promise) => {
+    console.error("Unhandled Rejection at:", promise, "reason:", reason);
+    Sentry.captureException(reason as Error);
+    process.exit(1);
+    })
+
+process.on("uncaughtException", (error) => {
+    console.error("Uncaught Exception thrown", error);
+    Sentry.captureException(error);
+    process.exit(1);
+    })  
+
+function main() {
+    const serverController = new ServerController();
+    void serverController.start();
+}
+
+main();
+
